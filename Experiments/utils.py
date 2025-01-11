fac04fdc87548a7c1839c763a897b477600118e8
invokerqwer <37833843+invokerqwer@users.noreply.github.com>
2025-01-11 15:10:01 +0800

    file_update

--- Experiments/utils.py
+++ Experiments/utils.py
@@ -9,14 +9,7 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
-
 def print_args(args):
-    """
-    打印参数对象的所有参数和对应的值
-    """
-    # 将args转换为字典
     args_dict = vars(args)
-
-    # 遍历字典并打印键值对
     for key, value in args_dict.items():
         print(key, "=", value)
